f6c41c895ddd651fd119428088b5ebf952258e6f
Farrhan <work.farrhan@gmail.com>
2024-02-04 22:07:28 +0800

    init

---
+++ disability-sports-shop/src/main/java/sg/com/disabilitysportsshop/disabilitysportsshop/DisabilitySportsShopApplication.java
@@ -0,0 +1,13 @@
+package sg.com.disabilitysportsshop.disabilitysportsshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisabilitySportsShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisabilitySportsShopApplication.class, args);
+	}
+
+}
---
+++ disability-sports-shop/src/test/java/sg/com/disabilitysportsshop/disabilitysportsshop/DisabilitySportsShopApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.com.disabilitysportsshop.disabilitysportsshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisabilitySportsShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
